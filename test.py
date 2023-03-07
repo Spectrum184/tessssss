c2ffdc1917cb879f9e2dc27703cfb6ef8c136d95
root <root@ip-172-31-17-222.ap-southeast-1.compute.internal>
2023-03-07 17:58:57 +0000

    first commit

---
+++ test.py
@@ -0,0 +1,33 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+import time
+
+# Number of pages to crawl
+num_pages = 900
+
+# URL template for Google search results
+url_template = 'https://www.google.com/search?q=site:https://en.ac-illust.com/creator-profile/&start={}'
+
+# List to store URLs
+urls = []
+
+# Crawl search result pages and extract URLs
+for i in range(num_pages):
+    url = url_template.format(i * 10)
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    print(i)
+    for a in soup.find_all('a'):
+        href = a.get('href')
+        if href.startswith('/url?q='):
+            url = href[7:].split('&')[0]
+            if "https://en.ac-illust.com/creator-profile" in url:
+                urls.append(url)
+
+    time.sleep(5)
+
+# Save URLs to a JSON file
+with open('urls.json', 'w') as f:
+    json.dump(urls, f)
+
